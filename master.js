b5619d8b735ccdf53402f3ae02404b6b3c030573
Tim Durward <timothy.a.durward@gmail.com>
2016-01-21 18:46:58 -0800

    add pingpong functionality

---
+++ master.js
@@ -0,0 +1,49 @@
+// Numbers divisible by 3 are replaced with "ping"
+// Numbers divisible by 5 are replaced with "pong"
+// Numbers divisible by 15 are replaced with "pingpong"
+
+
+function pingpong(num){
+
+    var j = 3;
+    var k = 5
+    var jk = 15
+
+    var i = 1;
+    while(i<=num) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log('pingpong');
+    } else if (i % 3 === 0) {
+      console.log('ping')
+    } else if (i % 5 === 0) {
+      console.log('pong');
+    }  else {
+      console.log(i);
+    }
+    i++;
+  }
+}
+
+
+function condensedPong(num){
+
+  function pingpong(num){
+
+      var ping = 3;
+      var pong = 5
+      var pingpong = 15
+
+      var i = 1;
+      while(i<=num) {
+      if (i % pingpong === 0){
+        console.log('pingpong');
+      } else if (i % ping === 0) {
+        console.log('ping')
+      } else if (i % pong === 0) {
+        console.log('pong');
+      }  else {
+        console.log(i);
+      }
+      i++;
+    }
+  }
